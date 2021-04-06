238dc1e0213b80d20f05d89e817e1d87563e3fa4
Rafael Maria <rafaelsmaria01@gmail.com>
2021-04-05 23:11:45 -0300

    configuração do projeto

---
+++ backend/src/test/java/com/unipampa/rp6/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipampa.rp6.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
